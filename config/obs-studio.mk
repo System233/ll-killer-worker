d08486c89bde2b7dfa4dc14304ad0dab0b15324e
System233 <20336040+System233@users.noreply.github.com>
2025-03-25 22:44:10 +0800

    测试

--- config/obs-studio.mk
+++ config/obs-studio.mk
@@ -1,4 +1,3 @@
-ARCH=amd64
 SOURCES=config/sources/ubuntu-noble.list
 BASE=org.deepin.base/23.1.0
 ARGS= CREATE_ARGS="--base $(BASE)" ENABLE_OSTREE=1 ENABLE_PTRACE=0
@@ -6,5 +5,9 @@ ARGS= CREATE_ARGS="--base $(BASE)" ENABLE_OSTREE=1 ENABLE_PTRACE=0
 REPO:=obsproject/obs-studio
 PKGID=obs-studio
 
-include common/base.mk
-include common/github.mk
+ifeq ($(ARCH),amd64);
+include config/strategy/base.mk
+include config/strategy/github.mk
+else
+index:
+endif
--- config/ubuntu-noble.mk
+++ config/ubuntu-noble.mk
@@ -1,7 +1,6 @@
-ARCH=amd64
 SOURCES=config/sources/ubuntu-noble.list
 BASE=org.deepin.base/23.1.0
 ARGS= CREATE_ARGS="--base $(BASE)" ENABLE_OSTREE=1 ENABLE_PTRACE=0
 
-include common/base.mk
-include common/index.mk
+include config/strategy/base.mk
+include config/strategy/index.mk
