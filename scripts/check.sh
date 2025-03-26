d6f08f0cf6ffa0cf51ea8f8426d5788a01bd3a6d
System233 <20336040+System233@users.noreply.github.com>
2025-03-27 01:58:17 +0800

    更新重建逻辑

--- scripts/check.sh
+++ scripts/check.sh
@@ -3,11 +3,13 @@
 PKG_INDEX=$1
 WORK_DIR="build/${ARCH}"
 trap exit SIGPIPE
+HASH=$(git rev-parse --short HEAD)
 while IFS=, read -r PKG VERSION SRC; do
     PKG_WORK_DIR="${WORK_DIR}/${PKG}"
     PKG_VERSION_FILE="${PKG_WORK_DIR}/version"
     PKG_VERSION=$(cat "$PKG_VERSION_FILE" 2>/dev/null)
-    if [ "$VERSION" != "$PKG_VERSION" ]; then
+    VERSION_HASH="$VERSION-$HASH"
+    if [ "$VERSION" != "$PKG_VERSION" ] && [ "$VERSION_HASH" != "$PKG_VERSION" ]; then
         echo "$PKG" 2>/dev/null
     fi
 done <"${PKG_INDEX}"
